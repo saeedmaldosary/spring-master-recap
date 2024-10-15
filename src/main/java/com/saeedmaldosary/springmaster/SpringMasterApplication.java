895a4411c0aabc8514df95c3a95e86c02e35e07a
Saeed Aldosary <saldosary@scai.sa>
2024-10-15 14:56:13 +0300

    Implement sort and limit using @RequestParam

--- src/main/java/com/saeedmaldosary/springmaster/SpringMasterApplication.java
+++ src/main/java/com/saeedmaldosary/springmaster/SpringMasterApplication.java
@@ -5,11 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @SpringBootApplication
@@ -21,6 +25,8 @@ public class SpringMasterApplication {
 
 	public enum Gender {MALE,FEMALE}
 
+	public enum SortingOrder {ASC,DESC}
+
 	public record Person(int id,String name, int age, Gender gender) {
 
 	}
@@ -29,19 +35,36 @@ public class SpringMasterApplication {
 
 	static {
 		people.add(new Person(1,"John",20,Gender.MALE));
+		people.add(new Person(2,"John",22,Gender.MALE));
 	}
 
 	@GetMapping
-	public List<Person> getPersons(){
-		return people;
+	public List<Person> getPersons(@RequestParam(value = "sort",required = false,defaultValue = "DESC")
+										 SortingOrder sort
+	,@RequestParam(value = "limit",required = false,defaultValue = "10")Integer limit){
+
+		Stream<Person> sortedStream;
+
+		if(sort == SortingOrder.ASC){
+			sortedStream =  people.stream().sorted(Comparator.comparing(Person::id));
+		} else {
+				sortedStream = people.stream().sorted(Comparator.comparing(Person::id).reversed());
+		}
+		return sortedStream.limit((limit)).collect(Collectors.toList());
 	}
 
-	/* In the context of Java (which this code appears to be),
+	/*
+	In the context of Java (which this code appears to be),
 	Optional is a container object introduced in Java 8.
 	 It's used to represent a value that may or may not be present.
-	 */
+	*/
+	/*
+	We can filter the data using @RequestParam
+	example: http://localhost:8080/1?sort=asc
+	*/
 	@GetMapping("{id}")
 	public Optional<Person> getPersonById(@PathVariable("id") Integer id){
+
 		return people.stream().filter(person -> person.id == id).findFirst();
 	}
 
