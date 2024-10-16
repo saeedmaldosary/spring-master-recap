65fff78d856d38bf00588100edf16b5fe4a9e24e
Saeed Aldosary <saldosary@scai.sa>
2024-10-16 07:52:25 +0300

    Use RequestMapping to implement different controllers

--- src/main/java/com/saeedmaldosary/springmaster/SpringMasterApplication.java
+++ src/main/java/com/saeedmaldosary/springmaster/SpringMasterApplication.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @RestController
+@RequestMapping("api/v1/person")
 @SpringBootApplication
 public class SpringMasterApplication {
 
